6d79b87b1dd40b74836ba6a9017fe741df3017b2
RKhizamboor <Ram.Khizamboor@gmail.com>
2019-10-30 17:34:14 +0530

    1st Commit

--- Words.py
+++ Words.py
@@ -2,10 +2,9 @@ from typing import List, Any
 from urllib.request import urlopen
 
 with urlopen('http://sixty-north.com/c/t.txt') as story:
-    story_words: List[Any]=[]
+    story_words: List[Any] = []
     for line in story:
         line_words = line.decode("utf-8").split()
         for word in line_words:
             story_words.append(word)
-print("Hello World")
 print(story_words)
