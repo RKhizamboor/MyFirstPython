d3362ea53075a015db4c2de2176e9e63eb1e3303
RKhizamboor <54762553+RKhizamboor@users.noreply.github.com>
2019-10-30 17:38:46 +0530

    Update Words.py

--- Words.py
+++ Words.py
@@ -7,4 +7,5 @@ with urlopen('http://sixty-north.com/c/t.txt') as story:
         line_words = line.decode("utf-8").split()
         for word in line_words:
             story_words.append(word)
+print("Hello World")
 print(story_words)
